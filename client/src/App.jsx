38956715e062d207a517c7272f7c4323287b6cb1
Sid1608 <akarsiddharth@gmail.com>
2022-02-06 10:45:31 +0530

    auth Component half completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {StreamChat} from 'stream-chat';
 import {Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie'
-import { ChannelContainer,ChannelListContainer } from './components';
+import { ChannelContainer,ChannelListContainer,Auth} from './components';
 import './App.css'
 
 
@@ -12,7 +12,10 @@ const apiKey='3nsmrbmwhfqt';
 // instance of a stream chat
 const client = StreamChat.getInstance(apiKey);
 
+const authToken=false;
 const App = () => {
+
+    if(!authToken) return <Auth/>
     return (
         <div className="app__wrapper">
             <Chat client={client} theme="team light">
