857ca42d263109b0b1f18b7dd82d0ca4d6e583a8
Sid1608 <akarsiddharth@gmail.com>
2022-02-11 20:13:53 +0530

    added twilio sms notification functionality

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
-
+const accountSid=process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid=process.env.TWILIO_Messaging_Service_Sid
+const twilioClient=require('twilio')(accountSid,authToken);
 
 /*allow to make cross origin requests*/
 app.use(cors());
@@ -17,7 +20,26 @@ app.use(express.urlencoded());
 app.get('/',(req,res)=>{
     res.send('Hello, world');
 })
-
+app.post('/',(req,res)=>{
+    const {message,user:sender,type,members}=req.body;
+    if(type==='message.new'){
+        members
+            .filter((member)=>member.user_id!=sender.id)
+            .foreach(({user})=>{
+                if(!user.online){
+                    twilioClient.messages.create({
+                        body:`You have a new message from ${message.user.fullName} - ${message.text}`,
+                        messagingServiceSid:messagingServiceSid,
+                        to:user.phoneNumber
+                    })
+                        .then(()=>console.log('Message sent!'))
+                        .catch((err)=>console.log(err));
+                }
+            })
+            return res.status(200).send('Message sent!');
+    }
+    return res.status(200).send('Not a new Message req!');
+})
 app.use('/auth',authRoutes)
 
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
